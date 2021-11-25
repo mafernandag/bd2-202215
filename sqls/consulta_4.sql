613817c7a2bb6705cc0bf5c8cd9f6efefde85bd9
María Guerra <mariafgg53@gmail.com>
2021-11-24 23:37:55 -0400

    Consulta 4

---
+++ sqls/consulta_4.sql
@@ -0,0 +1,11 @@
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('Administrador', 'Alcasa');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('gerente', 'JL1290 CA');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('almacenista', 'JL1290 CA');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('recepcionista', 'JL1290 CA');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('mensajero', 'JL1290 CA');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('manejo al cambio', 'El artesano');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('mensajero', 'El artesano');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('UX/UI designer', 'MyM San Félix');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('Contador', 'Sidor');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('supervisor', 'Palermo CA');
+INSERT INTO `consulta_4`(`nombre_cargo`, `nombre`) VALUES ('recepcionista', 'Palermo CA');
