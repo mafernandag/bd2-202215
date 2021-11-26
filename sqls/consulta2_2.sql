29b39658124663b44a1152fb39ce76733c2c520f
María Guerra <mariafgg53@gmail.com>
2021-11-25 21:23:17 -0400

    Consulta 2

---
+++ sqls/consulta2_2.sql
@@ -0,0 +1,6 @@
+INSERT INTO `consulta2_2`(`id_cargo`, `nombre_cargo`) VALUES ('2', 'supervisor');
+INSERT INTO `consulta2_2`(`id_cargo`, `nombre_cargo`) VALUES ('3', 'cajero');
+INSERT INTO `consulta2_2`(`id_cargo`, `nombre_cargo`) VALUES ('4', 'almacenista');
+INSERT INTO `consulta2_2`(`id_cargo`, `nombre_cargo`) VALUES ('5', 'recepcionista');
+INSERT INTO `consulta2_2`(`id_cargo`, `nombre_cargo`) VALUES ('6', 'manejo al cambio');
+INSERT INTO `consulta2_2`(`id_cargo`, `nombre_cargo`) VALUES ('7', 'mensajero');
