d0436f8f55cec8005f971257da26ebe86972c081
María Guerra <mariafgg53@gmail.com>
2021-11-29 23:55:12 -0400

    Cambios a la base de datos

--- sqls/actualizar_bd.sql
+++ sqls/actualizar_bd.sql
@@ -1,3 +1,5 @@
-ALTER TABLE `se_postula` ADD `fecha` date; 
-ALTER TABLE `registra` ADD `fecha` date; 
-ALTER TABLE `registra` ADD `fecha_vencimiento` date; 
+ALTER TABLE `bd2_202215_28162752`.`se_postula`
+ADD COLUMN `id_empresa` varchar(8) NOT NULL AFTER `fecha`,
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`id_cargo`, `cedula`, `id_empresa`) USING BTREE,  
+ADD CONSTRAINT `fk_se_postula_empresa_1` FOREIGN KEY (`id_empresa`) REFERENCES `bd2_202215_28162752`.`empresa` (`id_empresa`) ON DELETE RESTRICT ON UPDATE CASCADE;
