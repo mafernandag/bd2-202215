96af4c088ec8caf131c384526da9c083b422424b
María Guerra <mariafgg53@gmail.com>
2022-01-28 16:30:47 -0400

    Cambios a la base de datos
    Se agregó una columna nada más

--- sqls/actualizar_bd.sql
+++ sqls/actualizar_bd.sql
@@ -1,6 +1,2 @@
-ALTER TABLE `bd2_202215_28162752`.`registra`
-ADD COLUMN `candidato_seleccionado` varchar(8) AFTER `vacante_pagada`,
-ADD COLUMN `estado` char AFTER `candidato_seleccionado`;
-
-ALTER TABLE `bd2_202215_28162752`.`candidato`
-ADD COLUMN `codigo_validacion` varchar(10) AFTER `telefono`;
+ALTER TABLE se_postula
+ADD COLUMN expectativa_salarial FLOAT;
